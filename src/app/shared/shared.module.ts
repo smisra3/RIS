0957e1f64b6bdc553ee367dab1812863f734cb3d
Shubhankar Misra <smis64@sapient.com>
2017-08-09 15:50:24 +0530

    Adding the header and Footer Component.
    -Along with their routing.
    
    Thanks

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavigationBarComponent, SearchFormComponent],
-  exports: [NavigationBarComponent, SearchFormComponent]
+  declarations: [NavigationBarComponent, SearchFormComponent, HeaderComponent, FooterComponent],
+  exports: [NavigationBarComponent, SearchFormComponent, HeaderComponent, FooterComponent],
+  providers:[]
 })
 export class SharedModule { }
