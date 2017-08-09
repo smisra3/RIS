0b1962296f32838a6b11fd51d3d1c1b05afa9fb0
Shubhankar Misra <smis64@sapient.com>
2017-08-09 15:35:38 +0530

    Adding the new component
    -Search Form component added.
    - Along with it's sample routing.
    
    Thanks

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavigationBarComponent]
+  declarations: [NavigationBarComponent, SearchFormComponent],
+  exports: [NavigationBarComponent, SearchFormComponent]
 })
 export class SharedModule { }
