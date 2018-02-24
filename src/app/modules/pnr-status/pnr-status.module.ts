a84ec264e3bbbbc999c066574081d77b34c39231
Shubhankar Misra <shubhankarmisra.bit@gmail.com>
2018-02-24 18:17:44 +0530

    Adding the following features:
    -Adding the basic markup for each route;i.e. the home component have a
    structure for each route except for facts.
    -Adding the enhancement for seach form component.
    
    Thanks.

--- src/app/modules/pnr-status/pnr-status.module.ts
+++ src/app/modules/pnr-status/pnr-status.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PnrStatusRoutingModule } from './pnr-status-routing.module';
 import { MainComponent } from './main/main.component';
+import { SharedModule } from "app/shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    PnrStatusRoutingModule
+    PnrStatusRoutingModule,
+    SharedModule
   ],
   declarations: [MainComponent]
 })
